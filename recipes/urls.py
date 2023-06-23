53f294a514450441d23f28df9115c2e259624ba0
gustavoAlv3ss <luckbr6666@gmail.com>
2023-06-23 00:22:37 -0300

    aula66

--- recipes/urls.py
+++ recipes/urls.py
@@ -6,5 +6,8 @@ app_name = 'recipes'
 
 urlpatterns = [
     path('',  views.home, name="home"), # Passa o caminho(url) e uma função que vai ser executada # noqa
+    path('recipes/category/<int:category_id>/',  views.category, name="category"), # noqa
+
     path('recipes/<int:id>/',  views.recipe, name="recipe")
 ]
+
